46055c5773dd2d576be416da6c6baff5f5d059b9
spaceape3001 <spaceape3001@users.noreply.github.com>
2025-08-03 10:35:02 +0000

    Object Tree

--- exe/b3/debug.cpp
+++ exe/b3/debug.cpp
@@ -5,11 +5,17 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "b3.hpp"
+#include <b3/spatial/Point.hpp>
+#include <b3/spatial/PointLine.hpp>
+#include <b3/terrain/Terrain.hpp>
+
+#include <b3/Frame.hxx>
+
 #include <iostream>
 
 ////////////////////////////////////////////////////////////////////////////////
 
-bool    includedFiles(Doc& doc)
+static bool    includedFiles(Doc& doc)
 {
     for(auto& fp : doc.files())
         std::cout << fp.string() << "\n";
@@ -20,7 +26,7 @@ YQ_B3_HANDLER("files", "List files used during parse", includedFiles)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-bool    objCounter(Doc&)
+static bool    objCounter(Doc&)
 {
     std::cout << "Obj allocations is " << Obj::net_allocations();
     return true;
@@ -31,3 +37,40 @@ YQ_B3_HANDLER("objcnt", "Net number of objects created", objCounter)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+static void     writeTree(const Obj& obj, unsigned depth=0)
+{
+    for(unsigned n=1;n<depth;++n)
+        std::cout << "  ";
+    if(depth)
+        std::cout << "> ";
+    
+    std::cout << obj.metaInfo().stem() << " {" << obj.name() << "}";
+    
+    if(const Point* pt = dynamic_cast<const Point*>(&obj)){
+        std::cout << " {" << pt->define().x << ", " << pt->define().y << ", " << key_for(pt->type()) << " " << pt->define().z << "}";
+    }
+
+    if(const Terrain* t = dynamic_cast<const Terrain*>(&obj)){
+        
+    }
+    
+    if(const PointLine* pl = dynamic_cast<const PointLine*>(&obj)){
+        std::cout << " (" << pl->points(COUNT) << " points)";
+    }
+    
+    std::cout << '\n';
+    
+    if(const Frame* pt = dynamic_cast<const Frame*>(&obj)){
+        pt->children([&](const Obj& ch){
+            writeTree(ch, depth+1);
+        });
+    }
+}
+
+static bool     objectTree(Doc& doc)
+{
+    writeTree(doc);
+    return true;
+}
+
+YQ_B3_HANDLER("objtree", "Objects (tree hierarchy)", objectTree)
